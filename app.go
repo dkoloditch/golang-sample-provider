25eb0ef76d885d7892d2bec0045575e4fcfad077
Derek Koloditch <derek.koloditch@gmail.com>
2017-05-31 15:31:57 -0600

    some resource provisioning refactoring

--- app.go
+++ app.go
@@ -80,64 +80,15 @@ func resourcesHandler(w http.ResponseWriter, r *http.Request) {
   var rqs RequestStruct
   decoder.Decode(&rqs)
 
-  if !checkProduct(rqs.Product) {
-    resp := ResponseStruct{"bad product"}
-    js, err := json.Marshal(resp)
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-    }
+  if productIsNotValidAndResponseCreated(rqs.Product, w) { return }
 
-    w.WriteHeader(http.StatusBadRequest)
-    w.Write(js)
-    return
-  }
+  if planIsNotValidAndResponseCreated(rqs.Plan, w) { return }
 
-  if !checkPlan(rqs.Plan) {
-    resp := ResponseStruct{"bad plan"}
-    js, err := json.Marshal(resp)
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-    }
+  if regionIsNotValidAndResponseCreated(rqs.Region, w) { return }
 
-    w.WriteHeader(http.StatusBadRequest)
-    w.Write(js)
-    return
-  }
+  if resourceExistsAndResponseCreated(r.Method, rqs, w) { return }
 
-  if !checkRegion(rqs.Region) {
-    resp := ResponseStruct{"bad region"}
-    js, err := json.Marshal(resp)
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-    }
-
-    w.WriteHeader(http.StatusBadRequest)
-    w.Write(js)
-    return
-  }
-
-  if resourceExistsAndResponseCreated(r.Method, rqs, w) {
-    return
-  }
-
-  // get the random number and create json response
-  result := seed()
-  resp := ResponseStruct{fmt.Sprintf("%d", result)}
-  rqsData := RequestStruct{rqs.Id, rqs.Product, rqs.Plan, rqs.Region}
-  js, err := json.Marshal(resp)
-  data, err := json.Marshal(rqsData)
-
-  issueResponseIfErrorOccurs(err, w)
-
-  // add to db
-  db[rqs.Id] = string(data)
-
-  w.WriteHeader(http.StatusCreated)
-  w.Write(js)
-  return
+  if validRequestAndResponseCreated(rqs, w) { return }
 }
 
 func credentialsHandler(w http.ResponseWriter, r *http.Request) {
@@ -154,40 +105,46 @@ func seed() string {
   return fmt.Sprintf("%d", result)
 }
 
-func checkPlan(plan string) bool {
-  plans := [2]string{"small", "large"}
-
-  for i := range plans {
-    if plan == plans[i] {
-      return true
-    }
-  }
-
-  return false
-}
-
-func checkProduct(product string) bool {
+func productIsNotValidAndResponseCreated(product string, w http.ResponseWriter) bool {
   products := [1]string{"bonnets"}
 
   for i := range products {
     if product == products[i] {
-      return true
+      return false
     }
   }
 
-  return false
+  handleResponse("bad product", http.StatusBadRequest, w)
+
+  return true
 }
 
-func checkRegion(region string) bool {
+func planIsNotValidAndResponseCreated(plan string, w http.ResponseWriter) bool {
+  plans := [2]string{"small", "large"}
+
+  for i := range plans {
+    if plan == plans[i] {
+      return false
+    }
+  }
+
+  handleResponse("bad plan", http.StatusBadRequest, w)
+
+  return true
+}
+
+func regionIsNotValidAndResponseCreated(region string, w http.ResponseWriter) bool {
   regions  := [1]string{"aws::us-east-1"}
 
   for i := range regions {
     if region == regions[i] {
-      return true
+      return false
     }
   }
 
-  return false
+  handleResponse("bad region", http.StatusBadRequest, w)
+
+  return true
 }
 
 func resourceExistsAndResponseCreated(method string, rqs RequestStruct, w http.ResponseWriter) bool {
@@ -220,6 +177,37 @@ func resourceExistsAndResponseCreated(method string, rqs RequestStruct, w http.R
   }
 
   return false
+}
+
+func validRequestAndResponseCreated(rqs RequestStruct, w http.ResponseWriter) bool {
+  // @TODO: can this be abstracted further?
+  
+  // get the random number and create json response
+  result := seed()
+  resp := ResponseStruct{fmt.Sprintf("%d", result)}
+  rqsData := RequestStruct{rqs.Id, rqs.Product, rqs.Plan, rqs.Region}
+  js, err := json.Marshal(resp)
+  data, err := json.Marshal(rqsData)
+
+  issueResponseIfErrorOccurs(err, w)
+
+  // add to db
+  db[rqs.Id] = string(data)
+
+  w.WriteHeader(http.StatusCreated)
+  w.Write(js)
+
+  return true
+}
+
+func handleResponse(responseMessage string, statusCode int, w http.ResponseWriter) {
+  resp := ResponseStruct{responseMessage}
+  js, err := json.Marshal(resp)
+
+  issueResponseIfErrorOccurs(err, w)
+
+  w.WriteHeader(statusCode)
+  w.Write(js)
 }
 
 func convertRequestToJson(rqs RequestStruct) []byte {
